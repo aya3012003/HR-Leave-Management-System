d21d3d97472fa965c26e40b00c4bf625afacc844
aya3012003 <ayahassansayed1@gmail.com>
2026-07-17 01:35:47 +0300

    add missing AutoMapper mappings for EmployeeLeaveBalance

--- Project-3/src/Application/Mapping/MappingProfile.cs
+++ Project-3/src/Application/Mapping/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Project_3.src.Application.DTOs.DepartmentDTOs;
+using Project_3.src.Application.DTOs.EmployeeLeaveBalanceDtos;
 using Project_3.src.Application.DTOs.LeaveTypeDto;
 using Project_3.src.Application.Models;
 
@@ -12,10 +13,20 @@ namespace Project_3.src.Application.Mapping
             CreateMap<Department, DepartmentDto>().ReverseMap();
             CreateMap<CreateDepartmentDto, Department>().ReverseMap();
             CreateMap<UpdateDepartmentDto, Department>().ReverseMap();
+            CreateMap<EmployeeLeaveBalance, EmployeeLeaveBalanceDto>()
+                .ForMember(dest => dest.EmployeeName,
+                    opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
+                .ForMember(dest => dest.LeaveTypeName,
+                    opt => opt.MapFrom(src => src.LeaveType.Name));
+
+            CreateMap<CreateEmployeeLeaveBalanceDto, EmployeeLeaveBalance>();
+
+            CreateMap<UpdateEmployeeLeaveBalanceDto, EmployeeLeaveBalance>();
 
             CreateMap<LeaveType, LeaveTypeDto>();
             CreateMap<CreateLeaveTypeDto, LeaveType>();
             CreateMap<UpdateLeaveTypeDto, LeaveType>();
+
         }
 
     }
