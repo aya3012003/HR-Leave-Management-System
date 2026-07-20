2db428d3048f33a0aa24e6238f634c6fafab6690
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-07-20 21:16:46 +0300

    error in RequestTimingMiddleware

--- Project-3/src/API/Middleware/RequestTimingMiddleware.cs
+++ Project-3/src/API/Middleware/RequestTimingMiddleware.cs
@@ -15,7 +15,7 @@ namespace Project_3.src.API.Middleware
         public async Task InvokeAsync(HttpContext context)
         {
             var sw = Stopwatch.StartNew();
-            await _next(context);
+            //await _next(context);
             sw.Stop();
             _logger.LogInformation("{Method} {Path} → {Status} in {Ms}ms",
                    context.Request.Method, context.Request.Path,
